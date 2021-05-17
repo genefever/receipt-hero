728454208327102e455266d11c51ec497893a672
Gene Horecka <gene@genefever.com>
2021-05-17 14:10:26 -0700

    Create Header component.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggle"
+          aria-controls="navbarToggle"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <a className="navbar-brand" href="/">
+          receipt hero
+        </a>
+
+        <div className="collapse navbar-collapse" id="navbarToggle">
+          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+            <li className="nav-link">Dashboard</li>
+          </ul>
+        </div>
+        <div className="text-end">
+          <button className="btn btn-outline-light me-3">Log in</button>
+          <button className="btn btn-primary" type="button">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
