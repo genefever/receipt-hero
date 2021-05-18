8a9e8fca5b2598d771cdbc69669dd2ad15b75740
Gene Horecka <gene@genefever.com>
2021-05-18 12:20:02 -0700

    Added Links

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "../Header";
-import Login from "../Login";
-import SignUp from "../SignUp";
+import Home from "../../routes/Home";
+import Login from "../../routes/Login";
+import SignUp from "../../routes/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
     </Router>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -22,16 +23,22 @@ function Header() {
           <img src={logo} width="40" height="40" alt="logo" />
           receipt hero
         </a>
+
         <div className="collapse navbar-collapse" id="navbarToggle">
           <ul className="navbar-nav me-auto mb-2 mb-sm-0">
             <li className="nav-link">Dashboard</li>
           </ul>
         </div>
         <div className="text-end">
-          <button className="btn btn-outline-light me-3">Log in</button>
-          <button className="btn btn-primary" type="button">
-            Sign up
-          </button>
+          <Link to="/login">
+            <button className="btn btn-outline-light me-3">Log in</button>
+          </Link>
+
+          <Link to="/signup">
+            <button className="btn btn-primary" type="button">
+              Sign up
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
