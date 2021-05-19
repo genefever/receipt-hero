0485bafc0252b409f395e736814b06fba25025ae
Gene Horecka <gene@genefever.com>
2021-05-18 23:18:04 -0700

    Create GlobaStyle

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,18 +4,35 @@ import Home from "../../routes/Home";
 import Login from "../../routes/Login";
 import SignUp from "../../routes/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+      background-color: #f5f2f0; /* #f5eaea */
+      font-family: "Montserrat", sans-serif;
+    }
+
+    h1,h2,h3,h4,h5,h6 {
+      font-family: "Montserrat";
+      font-weight: bold;
+    }
+
+`;
 
 function App() {
   return (
-    <Router>
-      <NavigationBar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        {/* <Route component={NoMatch} /> */}
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+function Card() {}
+
+export default Card;
