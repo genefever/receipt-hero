b528fd538720611a98301926af587a97da9b178b
Gene Horecka <gene@genefever.com>
2021-05-21 14:54:39 -0700

    Print table works

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import cellEditFactory, { Type } from "react-bootstrap-table2-editor";
 import paginationFactory from "react-bootstrap-table2-paginator";
@@ -6,10 +6,18 @@ import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
-import { FaTrashAlt, FaFileCsv } from "react-icons/fa";
+import { FaTrashAlt, FaFileCsv, FaPrint } from "react-icons/fa";
 import { StyledButton } from "../components/Button";
+import ReactToPrint from "react-to-print";
+import Container from "react-bootstrap/Container";
+
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Table(props) {
+  const printComponentRef = useRef();
+
   const cellStyle = {
     whiteSpace: "nowrap",
     textOverflow: "ellipsis",
@@ -63,7 +71,7 @@ function Table(props) {
       props.onExport();
     };
     return (
-      <StyledButton variant="link" className="btn-sm" onClick={handleClick}>
+      <StyledButton className="btn-sm" onClick={handleClick}>
         <FaFileCsv /> Export
       </StyledButton>
     );
@@ -177,19 +185,34 @@ function Table(props) {
       {(props) => (
         <div>
           <h3>Title</h3>
-          <div className="d-flex justify-content-between pb-1">
-            <label>
-              Search:
-              <SearchBar
-                className="form-control-sm"
-                placeholder=""
-                {...props.searchProps}
-              />
-            </label>
-            <ExportCSVButton {...props.csvProps} />
-          </div>
-
-          <hr style={{ marginTop: "0", marginBottom: "0.75rem" }} />
+          <Container fluid>
+            <Row>
+              <Col xs={12} sm={6}>
+                <ButtonGroup>
+                  <ExportCSVButton {...props.csvProps} />
+                  <ReactToPrint
+                    trigger={() => (
+                      <StyledButton size="sm">
+                        <FaPrint /> Print
+                      </StyledButton>
+                    )}
+                    content={() => printComponentRef.current}
+                  />
+                </ButtonGroup>
+              </Col>
+              <Col xs={12} sm={6}>
+                <div className="float-right">
+                  <label>
+                    Search:
+                    <SearchBar
+                      className="form-control-sm"
+                      {...props.searchProps}
+                    />
+                  </label>
+                </div>
+              </Col>
+            </Row>
+          </Container>
 
           <BootstrapTable
             {...props.baseProps}
@@ -197,6 +220,7 @@ function Table(props) {
             pagination={pagination}
             cellEdit={cellEdit}
             bordered={false}
+            ref={printComponentRef}
           />
         </div>
       )}
