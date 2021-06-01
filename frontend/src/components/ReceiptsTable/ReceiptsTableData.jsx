266453c312cec4338a7e6cc6a21996ff1c563c63
Gene Horecka <gene@genefever.com>
2021-05-31 20:33:01 -0700

    Separate ReceiptsTableData from ReceiptsTable

---
+++ frontend/src/components/ReceiptsTable/ReceiptsTableData.jsx
@@ -0,0 +1,193 @@
+import { useContext, useState } from "react";
+import { Type } from "react-bootstrap-table2-editor";
+import { FaTrashAlt } from "react-icons/fa";
+import { ThemeContext } from "styled-components";
+import { StyledIconButtonSpan } from "../../components/Button";
+
+// Returns the formatted table data columns to populate BootstrapTable with.
+function ReceiptsTableData(props) {
+  const [meToPayTotal, setMeToPayTotal] = useState(0);
+  const [themToPayTotal, setThemToPayTotal] = useState(0);
+  const themeContext = useContext(ThemeContext);
+
+  function formatMonetaryCell(content) {
+    return content ? "$ " + parseFloat(content).toFixed(2) : "";
+  }
+
+  const cellStyle = {
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+    overflow: "hidden",
+  };
+
+  const footerStyle = {
+    whiteSpace: "normal",
+    wordWrap: "break-word",
+    fontWeight: "bold",
+  };
+
+  return [
+    {
+      dataField: "purchaseDate",
+      text: "Purchase Date",
+      type: "date",
+      style: cellStyle,
+      sort: true,
+      formatter: (cell) => {
+        let dateObj = cell;
+        if (typeof cell !== "object") {
+          dateObj = new Date(cell);
+        }
+        return `${("0" + (dateObj.getUTCMonth() + 1)).slice(-2)}/${(
+          "0" + dateObj.getUTCDate()
+        ).slice(-2)}/${dateObj.getUTCFullYear()}`;
+      },
+      csvFormatter: (cell) => {
+        let dateObj = cell;
+        if (typeof cell !== "object") {
+          dateObj = new Date(cell);
+        }
+        return `${("0" + (dateObj.getUTCMonth() + 1)).slice(-2)}/${(
+          "0" + dateObj.getUTCDate()
+        ).slice(-2)}/${dateObj.getUTCFullYear()}`;
+      },
+      editor: {
+        type: Type.DATE,
+      },
+      footer: "",
+    },
+    {
+      dataField: "storeName",
+      text: "Store Name",
+      style: cellStyle,
+      sort: true,
+      footer: "",
+    },
+    {
+      dataField: "buyer",
+      text: "Buyer",
+      style: cellStyle,
+      sort: true,
+      editor: {
+        type: Type.SELECT,
+        options: [
+          { value: "Me", label: "Me" },
+          { value: "Them", label: "Them" },
+        ],
+      },
+      footer: "Total:",
+    },
+    {
+      dataField: "total",
+      text: "Receipt Total",
+      type: "number",
+      sort: true,
+      style: cellStyle,
+      formatter: (cellContent) => {
+        return formatMonetaryCell(cellContent);
+      },
+      csvFormatter: (cellContent) => {
+        return formatMonetaryCell(cellContent);
+      },
+      csvType: Number,
+      footer: (columnData) => {
+        const receiptTotal = columnData.reduce(
+          (acc, item) => acc * 1 + item * 1,
+          0
+        );
+        return "$ " + parseFloat(receiptTotal).toFixed(2);
+      },
+    },
+    {
+      dataField: "meToPay",
+      text: "You Owe Them",
+      type: "number",
+      sort: true,
+      style: cellStyle,
+      formatter: (cellContent) => {
+        return formatMonetaryCell(cellContent);
+      },
+      csvFormatter: (cellContent) => {
+        return formatMonetaryCell(cellContent);
+      },
+      csvType: Number,
+      footer: (columnData) => {
+        const meToPayTotal = columnData.reduce(
+          (acc, item) => acc * 1 + item * 1,
+          0
+        );
+        setTimeout(() => {
+          setMeToPayTotal(meToPayTotal);
+        }, 0);
+        return "$ " + parseFloat(meToPayTotal).toFixed(2);
+      },
+      footerStyle: footerStyle,
+    },
+    {
+      dataField: "themToPay",
+      text: "They Owe You",
+      type: "number",
+      sort: true,
+      style: cellStyle,
+      formatter: (cellContent) => {
+        return formatMonetaryCell(cellContent);
+      },
+      csvFormatter: (cellContent) => {
+        return formatMonetaryCell(cellContent);
+      },
+      csvType: Number,
+      footer: (columnData) => {
+        const themToPayTotal = columnData.reduce(
+          (acc, item) => acc * 1 + item * 1,
+          0
+        );
+        setTimeout(() => {
+          setThemToPayTotal(themToPayTotal);
+        }, 0);
+        return "$ " + parseFloat(themToPayTotal).toFixed(2);
+      },
+      footerStyle: footerStyle,
+    },
+    {
+      dataField: "id",
+      text: "",
+      isDummyField: true,
+      align: "center",
+      editable: false,
+      searchable: false,
+      type: "number",
+      formatter: (cellContent, row) => {
+        return (
+          <StyledIconButtonSpan $delete onClick={() => props.onDelete(row.id)}>
+            <FaTrashAlt />
+          </StyledIconButtonSpan>
+        );
+      },
+      csvType: Number,
+      csvFormatter: (cellContent) => {
+        return "";
+      },
+      footer: () => {
+        const whoPaysMessage =
+          meToPayTotal > themToPayTotal ? "You owe them" : "They owe you";
+        const diff = Math.abs(themToPayTotal * 1 - meToPayTotal * 1);
+        return diff !== 0
+          ? whoPaysMessage + " $" + parseFloat(diff).toFixed(2)
+          : "";
+      },
+      footerAlign: "center",
+      footerStyle:
+        meToPayTotal > themToPayTotal
+          ? {
+              ...footerStyle,
+              backgroundColor: themeContext.cellColorYellow,
+            }
+          : {
+              ...footerStyle,
+              backgroundColor: themeContext.cellColorGreen,
+            },
+    },
+  ];
+}
+
+export default ReceiptsTableData;
---
+++ frontend/src/components/ReceiptsTable/index.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useRef } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import cellEditFactory from "react-bootstrap-table2-editor";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
+import { FaFileCsv, FaPrint } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+import { StyledButton, StyledIconButtonSpan } from "../../components/Button";
+import ReactToPrint from "react-to-print";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Input from "../Input";
+import emptyTable from "../../assets/empty-table.svg";
+import OutsideClickHandler from "react-outside-click-handler";
+import ReceiptsTableData from "./ReceiptsTableData";
+
+function ReceiptsTable(props) {
+  const [calculation, setCalculation] = useState({ title: "Untitled" });
+  const [editTitle, setEditTitle] = useState(false);
+
+  // Table properties
+  const columns = ReceiptsTableData(props);
+  const printComponentRef = useRef();
+  const { SearchBar } = Search;
+  const ExportCSVButton = (props) => {
+    const handleClick = () => {
+      props.onExport();
+    };
+    return (
+      <StyledButton className="btn-sm" variant="link" onClick={handleClick}>
+        <FaFileCsv /> Export
+      </StyledButton>
+    );
+  };
+  const cellEdit = cellEditFactory({
+    mode: "click",
+    afterSaveCell: (oldValue, newValue, row) => {
+      props.onEdit(row);
+    },
+  });
+
+  function toggleEditTitle() {
+    setEditTitle((prevEditTitle) => !prevEditTitle);
+  }
+
+  function handleTitleEdit(event) {
+    const value = event.target.value ? event.target.value : "Untitled";
+    setCalculation((prevValue) => {
+      return { ...prevValue, title: value };
+    });
+  }
+
+  return (
+    <ToolkitProvider
+      keyField="id"
+      bootstrap4={true}
+      data={props.receipts}
+      columns={columns}
+      exportCSV={{
+        fileName: "title.csv", // TODO: Pass in dynamic dashboard title,
+        ignoreFooter: false,
+      }}
+      search
+    >
+      {(props) => (
+        <div ref={printComponentRef}>
+          <div className="d-inline-flex">
+            {editTitle ? (
+              <OutsideClickHandler onOutsideClick={toggleEditTitle}>
+                <Input
+                  autoFocus
+                  defaultValue={calculation.title}
+                  handleChange={(e) => handleTitleEdit(e)}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") toggleEditTitle();
+                  }}
+                />
+              </OutsideClickHandler>
+            ) : (
+              <h4 onClick={toggleEditTitle}>{calculation.title}</h4>
+            )}
+            <StyledIconButtonSpan onClick={toggleEditTitle}>
+              <MdEdit className="ml-2" />
+            </StyledIconButtonSpan>
+          </div>
+
+          <Container fluid className="px-0 mb-3 hide-on-print">
+            <Row className="align-items-end">
+              <Col xs={12} sm={6}>
+                <div className="form-inline">
+                  <div className="form-group">
+                    <SearchBar
+                      className="form-control-sm"
+                      {...props.searchProps}
+                    />
+                  </div>
+                </div>
+              </Col>
+              <Col xs={12} sm={6}>
+                <div className="float-right">
+                  <ExportCSVButton {...props.csvProps} />
+                  <ReactToPrint
+                    trigger={() => (
+                      <StyledButton size="sm" variant="link">
+                        <FaPrint /> Print
+                      </StyledButton>
+                    )}
+                    content={() => printComponentRef.current}
+                    documentTitle={"Title"} // TODO: replace with dynamic title
+                  />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+
+          <BootstrapTable
+            {...props.baseProps}
+            cellEdit={cellEdit}
+            bordered={false}
+            condensed
+            noDataIndication={() => (
+              <div>
+                <h4 className="mt-4">
+                  {props.searchProps.searchText
+                    ? "No records found."
+                    : "Add a receipt to begin."}
+                </h4>
+                <img
+                  className="mx-auto d-block mt-3"
+                  src={emptyTable}
+                  width="270"
+                  height="270"
+                  alt="Empty table."
+                />
+              </div>
+            )}
+          />
+        </div>
+      )}
+    </ToolkitProvider>
+  );
+}
+
+export default ReceiptsTable;
